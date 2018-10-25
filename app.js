41654ce4a9bc81fb3c3dc6c3916e8c8d3d3216f3
Antoine GOSSET <antoine.gosset@ynov.com>
2018-10-25 15:34:19 +0200

    ajout de la date

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ $(function() {
     var end = moment();
 
     function cb(start, end) {
+        startDateReturn = start
+        endDateReturn = end
         $('#reportrange span').html(start.format('D/M/Y') + '-' + end.format('D/M/Y'));
     }
 
@@ -11,54 +13,22 @@ $(function() {
         startDate: start,
         endDate: end,
         ranges: {
-           'Today': [moment(), moment()],
-           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'Today': [moment().subtract(1,'days'), moment()],
+           'Yesterday': [moment().subtract(2, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(7, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(30, 'days'), moment()],
            'This Month': [moment().startOf('month'), moment().endOf('month')],
            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         }
     }, cb);
 
     cb(start, end);
-
+    
 });
 
 var apiURL = 'https://api.github.com/repos/'
-
-/**
- * Actual demo
- */
-
-
-/**var param = new Vue({
-
-    el: '#param',
-    data: {
-      selected: 'github-ynov-vue',
-      projets: [
-        { value: 'github-ynov-vue' },
-        { value: 'Airbnb-exercice' },
-        { value: 'Project 3' }
-      ],
-
-      users: [
-        { id:0 ,value: 'Killy85' },
-        { id:1 ,value: 'Nair0fl' },
-        { id:2 ,value: 'raphaelCharre' },
-        { id:3 ,value: 'mathiasLoiret' },
-        { id:4 ,value: 'AntoineGOSSET' },
-        { id:5 ,value: 'etienneYnov' }
-      ],
-
-      checkedNames: ['AntoineGOSSET'],
-    }
-  })*/
-
-
   
 var demo = new Vue({
-
 
   el: '#demo',
 
@@ -82,17 +52,19 @@ var demo = new Vue({
     { id:3 ,value: 'mathiasLoiret' },
     { id:4 ,value: 'AntoineGOSSET' },
     { id:5 ,value: 'etienneYnov' },
-    { id:6 ,value: 'KevinPautonnier'}
+    { id:6 ,value: 'KevinPautonnier'},
+    { id:7 ,value: 'AlexDesvallees'}
     ],
 
-    checkedNames: ['AntoineGOSSET'],  
+    checkedNames: [],  
       
-    resultliste: []
+    resultliste: [],
 
-  },
+    startDateReturn: null,
+    endDateReturn: null,
 
-  created: function () {
-    this.fetchData()
+    
+
   },
 
   watch: {
@@ -111,28 +83,20 @@ var demo = new Vue({
 
   methods: {
     fetchData: function () {
-        var xhr = new XMLHttpRequest()
-        var self = this
-        this.resultliste = []
-        this.checkedNames.forEach(function(name) {
-          console.log(name);
-          xhr.open('GET', apiURL + name + '/' + self.selected + '/commits?', false)
-          xhr.onload = function () {
-            self.commits = JSON.parse(xhr.responseText)
-            console.log("URL REQUEST:" + self.commits[0].html_url)
-            console.log("RESULT:" + self.commits)  
-            console.log("Commit :" + self.commits)
-            self.resultliste.push(self.commits)
-            console.log("LIST RESULT:" + self.resultliste)
-          }
-          
-          xhr.send() 
-          });
-          
+      console.log(startDateReturn)
+      console.log(endDateReturn)
+      var xhr = new XMLHttpRequest()
+      var self = this
+      this.resultliste = []
+      this.checkedNames.forEach(function(name) {
+        xhr.open('GET', apiURL + name + '/' + self.selected + '/commits?since='+ startDateReturn.format('YYYY-DD-MM') +'&until=' + endDateReturn.format('YYYY-DD-MM'), false)
+        xhr.onload = function () {
+          self.commits = JSON.parse(xhr.responseText)
+          self.resultliste.push(self.commits)
+        }
         
-        
-
-
+        xhr.send() 
+      });
     }
   }
 })
